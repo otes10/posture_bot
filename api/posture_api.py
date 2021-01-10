b11d41ff4feb05e93afab1b2d77d7d918c4f57b9
Tu1026 <s31302@gmail.com>
2021-01-10 02:38:56 -0800

    added s3 functionality into api and process

--- api/posture_api.py
+++ api/posture_api.py
@@ -1,28 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_restful import abort, Api, Resource
 from PIL import Image
+from aws import get_object, get_total_bytes
 
 import glob
 import json
+
 
 app = Flask(__name__)
 api = Api(app)
 
 @app.route("/current", methods=["POST"])
 def current_report():
-    filename = request.args.get('date','')
-    with open(f"/home/pi/nwhacks/api/data/{filename}.json") as f:
-        data = json.load(f)
-    
+    filename = request.args.get('date','') + '.json'
+    total_bytes = get_total_bytes(filename)
+    data = json.load(get_object(total_bytes, filename))
+    # with open(f"/home/pi/nwhacks/api/data/{filename}.json") as f:
+        # data = json.load(f)
     return jsonify({filename: data})
 
 @app.route("/hourly", methods=["POST"])
 def hourly_report():
     result = request.args
-    filename = result.get('date')
+    filename = result.get('date') + '.json'
     hour = result.get('hour')
     # # with open(f"/home/pi/nwhacks/api/data/{filename}.json") as f:
     # #     data = json.load(f)
+    total_bytes = get_total_bytes(filename)
+    data = json.load(get_object(total_bytes, filename))
     print(type(result))
     return jsonify({filename:hour})
 
